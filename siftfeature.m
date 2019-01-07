2f6958bcac55812d6ec5d73a373f7bbd90010f85
Baris Sevilmis <barissevilmis@Bariss-MacBook-Pro.local>
2019-01-07 19:55:56 +0300

    Indentation fixed

--- siftfeature.m
+++ siftfeature.m
@@ -64,12 +64,14 @@ for aa = 1:size(dog_space,2)
             neighof2_1window = neighof2_1(bb - k: bb + k, cc - k: cc + k);
             neighof2_2window = neighof2_2(bb - k: bb + k, cc - k: cc + k);
             
-            if (main2(bb, cc) == max(main2_window(:))) && (main2(bb, cc) >= max(neighof2_1window(:)))...
+            if (main2(bb, cc) == max(main2_window(:)))...
+                    && (main2(bb, cc) >= max(neighof2_1window(:)))...
                     && (main2(bb, cc) >= max(neighof2_2window(:)))
                 
                 coord = [coord; 2 aa bb cc sigma_arr(2,aa)];
                 
-            elseif (main2(bb, cc) == min(main2_window(:))) && (main2(bb, cc) <= min(neighof2_1window(:)))...
+            elseif (main2(bb, cc) == min(main2_window(:)))...
+                    && (main2(bb, cc) <= min(neighof2_1window(:)))...
                     && (main2(bb, cc) <= min(neighof2_2window(:)))
                 
                 coord = [coord; 2 aa bb cc sigma_arr(2,aa)];
@@ -81,12 +83,14 @@ for aa = 1:size(dog_space,2)
             neighof3_2window = neighof3_2(bb - k: bb + k, cc - k: cc + k);
             
             
-            if (main3(bb, cc) == max(main3_window(:))) && (main3(bb, cc) >= max(neighof3_1window(:)))...
+            if (main3(bb, cc) == max(main3_window(:)))...
+                    && (main3(bb, cc) >= max(neighof3_1window(:)))...
                     && (main3(bb, cc) >= max(neighof3_2window(:)))
                 
                 coord = [coord; 3 aa bb cc sigma_arr(3,aa)];
                 
-            elseif (main3(bb, cc) == min(main3_window(:))) && (main3(bb, cc) <= min(neighof3_1window(:)))...
+            elseif (main3(bb, cc) == min(main3_window(:)))...
+                    && (main3(bb, cc) <= min(neighof3_1window(:)))...
                     && (main3(bb, cc) <= min(neighof3_2window(:)))
                 
                 coord = [coord; 3 aa bb cc sigma_arr(3,aa)];
@@ -99,10 +103,6 @@ end
 %Display non-thresholded corner points
 disp(size(coord,1));
 figure;
-% imagesc(img); hold on;
-% [xramp,yramp] = meshgrid(1:1:size(dog_space{coord(:,1),coord(:,2)},1),1:1:size(dog_space{coord(:,1),coord(:,2)},1));
-% quiver(xramp,yramp,coord(:,4),coord(:,4),10,'r');
-% colormap gray;
 imshow(img);
 hold on;
 plot((2.^coord(:,2)-2).*coord(:,4), (2.^coord(:,2)-2).*coord(:,3) , 'b*');
@@ -115,26 +115,44 @@ for ll=1:size(coord,1)
     
     % Hesssian Matrix Values
     D = dog_space{coord(ll,1), coord(ll,2)};
-    Dx = (D(coord(ll,3), coord(ll,4) + 1) - D(coord(ll,3), coord(ll,4) - 1))/2;
-    Dy = (D(coord(ll,3) + 1, coord(ll,4)) - D(coord(ll,3) - 1, coord(ll,4)))/2;
-    Ds = (dog_space{coord(ll,1) + 1, coord(ll,2)}(coord(ll,3), coord(ll,4))...
-        - dog_space{coord(ll,1) - 1, coord(ll,2)}(coord(ll,3), coord(ll,4)))/2;
-    Dxx = D(coord(ll, 3), coord(ll, 4) + 1) + D(coord(ll, 3), coord(ll, 4) - 1)...
+    Dx = (D(coord(ll,3), coord(ll,4) + 1)...
+        - D(coord(ll,3), coord(ll,4) - 1))/2;
+    Dy = (D(coord(ll,3) + 1, coord(ll,4))...
+        - D(coord(ll,3) - 1, coord(ll,4)))/2;
+    Ds = (dog_space{coord(ll,1) + 1, coord(ll,2)}...
+        (coord(ll,3), coord(ll,4))...
+        - dog_space{coord(ll,1) - 1, coord(ll,2)}...
+        (coord(ll,3), coord(ll,4)))/2;
+    Dxx = D(coord(ll, 3), coord(ll, 4) + 1)...
+        + D(coord(ll, 3), coord(ll, 4) - 1)...
         - (2*D(coord(ll, 3), coord(ll, 4)));
-    Dyy = D(coord(ll, 3) + 1, coord(ll, 4)) + D(coord(ll, 3) - 1, coord(ll, 4))...
+    Dyy = D(coord(ll, 3) + 1, coord(ll, 4))...
+        + D(coord(ll, 3) - 1, coord(ll, 4))...
         - (2*D(coord(ll, 3), coord(ll, 4)));
-    Dxy = (D(coord(ll, 3) + 1, coord(ll, 4) + 1) - D(coord(ll, 3) - 1, coord(ll, 4) + 1)...
-        -D(coord(ll, 3) + 1, coord(ll, 4) - 1) + D(coord(ll, 3) - 1, coord(ll, 4) - 1))/4;
-    Dxs = (dog_space{coord(ll,1) + 1, coord(ll,2)}(coord(ll, 3), coord(ll, 4) + 1)...
-        - dog_space{coord(ll,1) + 1, coord(ll,2)}(coord(ll, 3), coord(ll, 4) - 1)...
-        - dog_space{coord(ll,1) - 1, coord(ll,2)}(coord(ll, 3), coord(ll, 4) + 1)...
-        + dog_space{coord(ll,1) - 1, coord(ll,2)}(coord(ll, 3), coord(ll, 4) - 1))/4;
-    Dys = (dog_space{coord(ll,1) + 1, coord(ll,2)}(coord(ll, 3) + 1, coord(ll, 4))...
-        - dog_space{coord(ll,1) + 1, coord(ll,2)}(coord(ll, 3) - 1, coord(ll, 4))...
-        - dog_space{coord(ll,1) - 1, coord(ll,2)}(coord(ll, 3) + 1, coord(ll, 4))...
-        + dog_space{coord(ll,1) - 1, coord(ll,2)}(coord(ll, 3) - 1, coord(ll, 4)))/4;
-    Dss = (dog_space{coord(ll,1) + 1, coord(ll,2)}(coord(ll, 3), coord(ll, 4))...
-        + dog_space{coord(ll,1) - 1, coord(ll,2)}(coord(ll, 3), coord(ll, 4)))...
+    Dxy = (D(coord(ll, 3) + 1, coord(ll, 4) + 1)...
+        - D(coord(ll, 3) - 1, coord(ll, 4) + 1)...
+        -D(coord(ll, 3) + 1, coord(ll, 4) - 1)...
+        + D(coord(ll, 3) - 1, coord(ll, 4) - 1))/4;
+    Dxs = (dog_space{coord(ll,1) + 1, coord(ll,2)}...
+        (coord(ll, 3), coord(ll, 4) + 1)...
+        - dog_space{coord(ll,1) + 1, coord(ll,2)}...
+        (coord(ll, 3), coord(ll, 4) - 1)...
+        - dog_space{coord(ll,1) - 1, coord(ll,2)}...
+        (coord(ll, 3), coord(ll, 4) + 1)...
+        + dog_space{coord(ll,1) - 1, coord(ll,2)}...
+        (coord(ll, 3), coord(ll, 4) - 1))/4;
+    Dys = (dog_space{coord(ll,1) + 1, coord(ll,2)}...
+        (coord(ll, 3) + 1, coord(ll, 4))...
+        - dog_space{coord(ll,1) + 1, coord(ll,2)}...
+        (coord(ll, 3) - 1, coord(ll, 4))...
+        - dog_space{coord(ll,1) - 1, coord(ll,2)}...
+        (coord(ll, 3) + 1, coord(ll, 4))...
+        + dog_space{coord(ll,1) - 1, coord(ll,2)}...
+        (coord(ll, 3) - 1, coord(ll, 4)))/4;
+    Dss = (dog_space{coord(ll,1) + 1, coord(ll,2)}...
+        (coord(ll, 3), coord(ll, 4))...
+        + dog_space{coord(ll,1) - 1, coord(ll,2)}...
+        (coord(ll, 3), coord(ll, 4)))...
         - (2*D(coord(ll, 3), coord(ll, 4)));
     
     %Clairaut's Theorem: Dxy == Dyx iff. both are continuous within
@@ -147,14 +165,15 @@ for ll=1:size(coord,1)
     Taylor_dog = D(coord(ll, 3), coord(ll, 4)) + ((D_dif')*loc_ext)/2;
     
     if (abs(Taylor_dog) > threshold_1) && (max(abs(loc_ext)) < 0.5)
-        coord2 = [coord2; coord(ll,:) (coord(ll,3)+loc_ext(1)) (coord(ll,4)+loc_ext(2)) (coord(ll,5)+loc_ext(3)) Taylor_dog];
+        coord2 = [coord2; coord(ll,:) (coord(ll,3)+loc_ext(1))...
+            (coord(ll,4)+loc_ext(2)) (coord(ll,5)+loc_ext(3)) Taylor_dog];
     end
     
 end
 
 %Display first_level-thresholded corner points
 disp(size(coord2, 1));
-plot((2.^coord2(:,2)-2).*coord2(:,4), (2.^coord2(:,2)-2).*coord2(:,3) , 'g+');
+plot((2.^coord2(:,2)-2).*coord2(:,4),(2.^coord2(:,2)-2).*coord2(:,3),'g+');
 
 %% Eliminate Low Contrast Extremum
 
@@ -162,7 +181,8 @@ coord3 = [];
 for ll = 1:size(coord2,1)
     D = dog_space{coord2(ll,1), coord2(ll,2)};
     C_dog = 0.015;
-    if (abs(coord2(ll, 9)) >= C_dog) && (abs(D(coord2(ll, 3), coord2(ll, 4))) >=  0.8*C_dog)
+    if (abs(coord2(ll, 9)) >= C_dog)...
+            && (abs(D(coord2(ll, 3), coord2(ll, 4))) >=  0.8*C_dog)
         coord3 = [coord3; coord2(ll, :)];
     end
 end
@@ -173,11 +193,15 @@ coord_final = [];
 for ll=1:size(coord3,1)
     
     D = dog_space{coord3(ll,1), coord3(ll,2)};
-    Dxx_new = D(coord3(ll, 3), coord3(ll, 4) + 1) + D(coord3(ll, 3), coord3(ll, 4) - 1)...
+    Dxx_new = D(coord3(ll, 3), coord3(ll, 4) + 1)...
+        + D(coord3(ll, 3), coord3(ll, 4) - 1)...
         - (2*D(coord3(ll, 3), coord2(ll, 4)));
-    Dxy_new = (D(coord3(ll, 3) + 1, coord3(ll, 4) + 1) - D(coord3(ll, 3) - 1, coord3(ll, 4) + 1)...
-        -D(coord3(ll, 3) + 1, coord3(ll, 4) - 1) + D(coord3(ll, 3) - 1, coord3(ll, 4) - 1))/4;
-    Dyy_new = D(coord3(ll, 3) + 1, coord3(ll, 4)) + D(coord3(ll, 3) - 1, coord3(ll, 4))...
+    Dxy_new = (D(coord3(ll, 3) + 1, coord3(ll, 4) + 1)...
+        - D(coord3(ll, 3) - 1, coord3(ll, 4) + 1)...
+        -D(coord3(ll, 3) + 1, coord3(ll, 4) - 1)...
+        + D(coord3(ll, 3) - 1, coord3(ll, 4) - 1))/4;
+    Dyy_new = D(coord3(ll, 3) + 1, coord3(ll, 4))...
+        + D(coord3(ll, 3) - 1, coord3(ll, 4))...
         - (2*D(coord3(ll, 3), coord3(ll, 4)));
     
     %Hessian 2x2
@@ -193,7 +217,8 @@ for ll=1:size(coord3,1)
     end
 end
 disp(size(coord_final,1));
-plot((2.^coord_final(:,2)-2).*coord_final(:,4),(2.^coord_final(:,2)-2).*coord_final(:,3) , 'mo');
+plot((2.^coord_final(:,2)-2).*coord_final(:,4),...
+    (2.^coord_final(:,2)-2).*coord_final(:,3) , 'mo');
 hold off;
 %% Magnitude and Theta Space
 mag_space = cell(5,4);
@@ -201,20 +226,27 @@ theta_space = cell(5,4);
 offset = 50;
 for ii = 1:size(scale_space,2)
     for jj=1:size(scale_space,1)
-        mag_space{jj, ii} = zeros(size(scale_space{jj,ii},1),size(scale_space{jj,ii},2));
-        theta_space{jj, ii} = zeros(size(scale_space{jj,ii},1),size(scale_space{jj,ii},2));
+        mag_space{jj, ii} = zeros(size(scale_space{jj,ii},1),...
+            size(scale_space{jj,ii},2));
+        theta_space{jj, ii} = zeros(size(scale_space{jj,ii},1),...
+            size(scale_space{jj,ii},2));
         I = scale_space{jj,ii};
         for xx=2:size(mag_space{jj, ii},1)-1
             for yy=2:size(mag_space{jj, ii},2)-1
-                mag_space{jj, ii}(xx,yy) = sqrt(((I(xx + 1, yy)-I(xx -1,yy))^2)...
+                mag_space{jj, ii}(xx,yy) = ...
+                    sqrt(((I(xx + 1, yy)-I(xx -1,yy))^2)...
                     + ((I(xx,yy+1)-I(xx,yy-1))^2));
-                theta_space{jj, ii}(xx,yy) = atan2d((I(xx,yy+1)-I(xx,yy-1))...
+                theta_space{jj, ii}(xx,yy) = ...
+                    atan2d((I(xx,yy+1)-I(xx,yy-1))...
                     , (I(xx+1,yy)-I(xx-1,yy)));
-                theta_space{jj, ii}(xx,yy) = mod(theta_space{jj, ii}(xx,yy) + 360,360);
+                theta_space{jj, ii}(xx,yy) = ...
+                    mod(theta_space{jj, ii}(xx,yy) + 360,360);
             end
         end
-        mag_space{jj, ii} = padarray(mag_space{jj, ii}, [offset offset],'both');
-        theta_space{jj, ii} = padarray(theta_space{jj, ii}, [offset offset],'both');
+        mag_space{jj, ii} = padarray(mag_space{jj, ii},...
+            [offset offset],'both');
+        theta_space{jj, ii} = padarray(theta_space{jj, ii},...
+            [offset offset],'both');
     end
 end
 %% Orientation Assignment
@@ -231,20 +263,26 @@ for oo=1:size(coord_final,1)
     if mod(winsize,2) == 0
         winsize = winsize + 1;
     end
-    gauss_filter = fspecial('gaussian', [winsize winsize], 1.5*coord_final(oo,8));
+    gauss_filter = fspecial('gaussian',...
+        [winsize winsize], 1.5*coord_final(oo,8));
     currx = coord_final(oo,3) ;%* (2^(coord_final(oo, 2) -idy)); 
     curry = coord_final(oo,4) ;%* (2^(coord_final(oo, 2) -idy));
     
-    theta_win = theta_I(currx-(winsize-1)/2+offset:currx+(winsize-1)/2+offset,...
-        curry-(winsize-1)/2+offset:curry+(winsize-1)/2+offset);
-    mag_win = mag_I(currx-(winsize-1)/2+offset:currx+(winsize-1)/2+offset,...
-        curry-(winsize-1)/2+offset:curry+(winsize-1)/2+offset);
+    theta_win = theta_I(currx-(winsize-1)/2+offset:...
+        currx+(winsize-1)/2+offset,...
+        curry-(winsize-1)/2+offset:...
+        curry+(winsize-1)/2+offset);
+    mag_win = mag_I(currx-(winsize-1)/2+offset:...
+        currx+(winsize-1)/2+offset,...
+        curry-(winsize-1)/2+offset:...
+        curry+(winsize-1)/2+offset);
     mag_win = mag_win .* gauss_filter;
     for ii = 1:size(theta_win,1)
         for jj = 1:size(theta_win,2)
             theta = mod(theta_win(ii,jj)+360,360);
             m = mag_win(ii,jj);
-            hist_bin(1, floor(theta/10)+1) = hist_bin(1, floor(theta/10)+1) + m;
+            hist_bin(1, floor(theta/10)+1) = ...
+                hist_bin(1, floor(theta/10)+1) + m;
         end
         
     end
@@ -265,7 +303,8 @@ for oo=1:size(coord_final,1)
             %Interpolation
             [p,S,mu]  = polyfit([Y(1) Y(2) Y(3)], [X(1) X(2) X(3)], 2); 
             dir = polyval(p,Y(2),[],mu)*10;
-            coord_final2 = [coord_final2; coord_final(oo,:) idx idy hist_bin(aa) dir];
+            coord_final2 = ...
+                [coord_final2; coord_final(oo,:) idx idy hist_bin(aa) dir];
         end
     end
 end
@@ -285,46 +324,49 @@ for oo = 1:size(coord_final2,1)
     theta = coord_final2(oo,13);
      
     % Lowe descriptor
-%     %New approach
-%     coor = [subpixel_idx+offset; subpixel_idy+offset];
-%     size_mag = [size(mag_I,1)/2; size(mag_I,2)/2];
-%     
-%     theta = 360 - theta;
-%     rotMag = imrotate(mag_I, theta);
-%     rotTheta = imrotate(theta_I, theta);
-%     
-%     rotMat = [cosd(theta) -sind(theta); sind(theta) cosd(theta)];
-%     temp_coor = rotMat*(coor - size_mag);
-%     rot_coor = temp_coor + size_mag;
-%     
-%     difMat = [(size(rotMag,1) - size(mag_I,1))/2; (size(rotMag,2) - size(mag_I,2))/2];
-%     rot_coor_final = rot_coor + difMat;
-%     rot_row = round(rot_coor_final(1));
-%     rot_col = round(rot_coor_final(2));
-%     
-%     temp_vec = zeros(4,4,8);
-%     %Create window
-%     for i = 1:1:16
-%         for j = 1:1:16      
-%             rowTrue = round(rot_row + j - 9);
-%             colTrue = round(rot_col + i - 9);
-%             
-%             %Check if points between boundaries
-%             if rowTrue > 0 && colTrue > 0 && rowTrue <= size(rotMag, 1) && colTrue <= size(rotMag, 2)
-%                 mag = rotMag(rowTrue, colTrue)* G1(j,i);
-%                 ori = mod(rotTheta(rowTrue, colTrue) + 360, 360);
-%                 temp_vec(ceil(i/4),ceil(j/4),floor(ori/45)+1) = temp_vec(ceil(i/4),ceil(j/4),floor(ori/45)+1) + mag;
-%             end
-%         end
-%     end
-%     
-%     count = 1;
-%     for i = 1:1:4
-%         for j = 1:1:4
-%             feature_vec(oo,count:count+7) = temp_vec(i,j,:);
-%             count = count + 8;
-%         end
-%     end
+    %New approach
+    coor = [subpixel_idx+offset; subpixel_idy+offset];
+    size_mag = [size(mag_I,1)/2; size(mag_I,2)/2];
+    
+    theta = 360 - theta;
+    rotMag = imrotate(mag_I, theta);
+    rotTheta = imrotate(theta_I, theta);
+    
+    rotMat = [cosd(theta) -sind(theta); sind(theta) cosd(theta)];
+    temp_coor = rotMat*(coor - size_mag);
+    rot_coor = temp_coor + size_mag;
+    
+    difMat = [(size(rotMag,1) - size(mag_I,1))/2;...
+        (size(rotMag,2) - size(mag_I,2))/2];
+    rot_coor_final = rot_coor + difMat;
+    rot_row = round(rot_coor_final(1));
+    rot_col = round(rot_coor_final(2));
+    
+    temp_vec = zeros(4,4,8);
+    %Create window
+    for i = 1:1:16
+        for j = 1:1:16      
+            rowTrue = round(rot_row + j - 9);
+            colTrue = round(rot_col + i - 9);
+            
+            %Check if points between boundaries
+            if rowTrue > 0 && colTrue > 0 &&...
+                    rowTrue <= size(rotMag, 1) && colTrue <= size(rotMag,2)
+                mag = rotMag(rowTrue, colTrue)* G1(j,i);
+                ori = mod(rotTheta(rowTrue, colTrue) + 360, 360);
+                temp_vec(ceil(i/4),ceil(j/4),floor(ori/45)+1) =...
+                    temp_vec(ceil(i/4),ceil(j/4),floor(ori/45)+1) + mag;
+            end
+        end
+    end
+    
+    count = 1;
+    for i = 1:1:4
+        for j = 1:1:4
+            feature_vec(oo,count:count+7) = temp_vec(i,j,:);
+            count = count + 8;
+        end
+    end
     
 %AI Shack
     %Current approach
@@ -350,9 +392,9 @@ for oo = 1:size(coord_final2,1)
                     theta_bin = theta_f(k,l) - theta + (180/8) + 360;
                     theta_bin = mod(theta_bin,360);
                     
-                    %Trilinear interpolation
-                    %trilin = 1 - ((((floor(theta_bin/45)*45)-22.5)-theta_bin)/pi);
-                    feature_vec(oo, count+floor(theta_bin/45)) = feature_vec(oo, count+floor(theta_bin/45))...
+                    %Trilinear interpolation                 
+                    feature_vec(oo, count+floor(theta_bin/45)) =...
+                        feature_vec(oo, count+floor(theta_bin/45))...
                         + mag_f(k,l);
                     
                 end
